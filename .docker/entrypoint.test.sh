b9b42884715ea54e38efe907d83df20644bb09b5
Jeremy Levy <jje.levy@gmail.com>
2020-07-14 12:06:10 +0200

    use bash in entrypoints

--- .docker/entrypoint.test.sh
+++ .docker/entrypoint.test.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+if [[ $DATABASE_URL != *_test ]]; then
+  echo "Database must be a test database";
+  exit 1;
+fi
 
 php bin/console doctrine:database:create --if-not-exists --no-debug
 php bin/console doctrine:migrations:migrate --no-interaction --no-debug
